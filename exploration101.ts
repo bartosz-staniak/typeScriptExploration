db8e7f6fc8147a9616545c36b023157e064818ed
bs <bartosz.staniak@gmail.com>
2024-06-13 14:57:10 +0200

    attempt to assign a value to the element

--- exploration101.ts
+++ exploration101.ts
@@ -2,4 +2,6 @@ function showText() {
     var textDisplayParagraph = document.getElementById("textDisplay");
     
     var textToDisplay = "testing";
+
+    textDisplayParagraph = textToDisplay;
 }
