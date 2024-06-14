5bdc8f7916b84f8beae5cb2cd283e9aeda3ede09
bs <bartosz.staniak@gmail.com>
2024-06-15 01:51:56 +0200

    compiled changed ts to js

--- exploration101.js
+++ exploration101.js
@@ -1,5 +1,7 @@
+"use strict";
 var _a;
-var textDisplayParagraph = (_a = document.getElementById("textDisplay")) === null || _a === void 0 ? void 0 : _a.textContent;
+Object.defineProperty(exports, "__esModule", { value: true });
+var textDisplayParagraph = (_a = document.getElementById("textDisplay")) === null || _a === void 0 ? void 0 : _a.innerHTML;
 function showText() {
     var textToDisplay = "testing";
     textDisplayParagraph = textToDisplay;
