3c9a29a4a151a1d338b3fd27698cdfa1c1c1964a
bs <bartosz.staniak@gmail.com>
2024-06-14 15:55:51 +0200

    generated the js file after function name change in the ts file

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var _a;
 var textDisplayParagraph = (_a = document.getElementById("textDisplay")) === null || _a === void 0 ? void 0 : _a.textContent;
-function showText() {
+function showText_ts() {
     var textToDisplay = "testing";
     textDisplayParagraph = textToDisplay;
 }
