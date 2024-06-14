5ffdfef1353c9b4b0ce053968eac0852611cbb50
bs <bartosz.staniak@gmail.com>
2024-06-15 00:47:43 +0200

    added an autogenerated js file

---
+++ exploration101.js
@@ -0,0 +1,6 @@
+var _a;
+var textDisplayParagraph = (_a = document.getElementById("textDisplay")) === null || _a === void 0 ? void 0 : _a.textContent;
+function showText() {
+    var textToDisplay = "testing";
+    textDisplayParagraph = textToDisplay;
+}
