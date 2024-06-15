4d125c23efb18d4de74816fc5c999452651f7ae9
bs <bartosz.staniak@gmail.com>
2024-06-16 01:08:31 +0200

    generated a js from ts

--- exploration101.js
+++ exploration101.js
@@ -3,7 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // yet produces in the browser logs a "ReferenceError: exports is not defined"
 function showText() {
     var textDisplayDiv = document.getElementById("textDisplay");
-    var textToDisplay = "testing";
+    var textToDisplay; // = "testing";
+    textToDisplay = "testing";
+    textToDisplay = 3;
     if (textDisplayDiv)
         textDisplayDiv.innerHTML = textToDisplay;
 }
