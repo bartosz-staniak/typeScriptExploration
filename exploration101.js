76d03b37e9e47b6adcdc91aa463a9cfe418bfa1c
bs <bartosz.staniak@gmail.com>
2024-06-15 03:36:40 +0200

    generated the js from updated ts

--- exploration101.js
+++ exploration101.js
@@ -1,4 +1,5 @@
-// export{} //solves duplicate function error
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 function showText() {
     var textDisplayDiv = document.getElementById("textDisplay");
     var textToDisplay = "testing";
