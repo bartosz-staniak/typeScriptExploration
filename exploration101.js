de09dae0d2e3591f85c621a5a99e823db505dc25
bs <bartosz.staniak@gmail.com>
2024-06-15 03:39:12 +0200

    generated a new js file after ts update

--- exploration101.js
+++ exploration101.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// yet produces in the browser logs a "ReferenceError: exports is not defined"
 function showText() {
     var textDisplayDiv = document.getElementById("textDisplay");
     var textToDisplay = "testing";
