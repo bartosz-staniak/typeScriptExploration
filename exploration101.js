754d3f51ae2adc5343e1fe531714cb9c40629fee
bs <bartosz.staniak@gmail.com>
2024-06-15 02:00:44 +0200

    built ts into js file

--- exploration101.js
+++ exploration101.js
@@ -1,6 +1,5 @@
-"use strict";
+// export{} //solves duplicate function error
 var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
 var textDisplayParagraph = (_a = document.getElementById("textDisplay")) === null || _a === void 0 ? void 0 : _a.innerHTML;
 function showText() {
     var textToDisplay = "testing";
