a4fc522d1ce6541788b6cff837cc4411d877e8a4
bs <bartosz.staniak@gmail.com>
2024-06-30 08:55:45 +0200

    commented out the check

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -4,7 +4,8 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-
+/*
 test ('check a string variable', () => {
   expect(showText2_check);  //failed attempt to find the name of the function in brackets
 });
+*/
