c687b9543b00548485976592cd365b7ac49fc244
bs <bartosz.staniak@gmail.com>
2024-07-02 23:42:18 +0200

    removed the comment

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -34,6 +34,6 @@ test('two plus two is not four', () => { // fails by design
   expect(2 + 2).toBe(!4);
 });
 
-test('two plus two is not four', () => { // fails by design
+test('two plus two is not four', () => {
   expect(1 + 1).toBe(!4);
 });
