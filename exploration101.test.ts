333fcde32166a6a18c7d3c6fe81b468307b7ebeb
bs <bartosz.staniak@gmail.com>
2024-07-06 11:14:27 +0200

    updated a comment

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -30,7 +30,7 @@ test ('six multiplied by two is 12', () => { // Duplicated the check to observe 
   expect(6 * 2).toBe(12);
 });
 
-test('two plus two is not four', () => { // fails by design
+test('two plus two is not four', () => { // fails by design (reason for failing: expected boolean when received number)
   expect(2 + 2).toBe(!4);
 });
 
