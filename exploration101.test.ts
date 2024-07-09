0e8bcd44a52db7c1e50a739032010cc9ae2d4367
bs <bartosz.staniak@gmail.com>
2024-07-09 10:06:00 +0200

    reverted last change

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -61,5 +61,5 @@ test('one plus two is not four', () => {
 
 
 test('string contains the word computer', () => {
-  expect('This will be read by the computr').toContain('computer');
+  expect('This will be read by the computer').toContain('computer');
 });
