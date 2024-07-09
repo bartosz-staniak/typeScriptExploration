67c48205bd06e1a524e7f59cb991d4bf65531ba8
bs <bartosz.staniak@gmail.com>
2024-07-09 11:00:00 +0200

    removed the affix and suffix from received computer word

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -61,7 +61,7 @@ test('one plus two is not four', () => {
 
 
 test('string contains the word computer', () => {
-  expect('This will be read by thecomputer!').toContain('computer');
+  expect('This will be read by computer').toContain('computer');
 });
 
 
