ff0def305e362c03d115f42bc196e4f2d1836939
bs <bartosz.staniak@gmail.com>
2024-07-09 07:58:30 +0200

    added empty lines in order to have test results presented without confusing partial presence of the consecutive test instruction

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -14,29 +14,36 @@ test ('six multiplied by two is 12', () => { // Duplicated the check to observe 
   expect(6 * 2).toBe(12);
 });
 
+
 test('two plus two is four', () => {
   expect(2 + 2).toBe(4);
 });
+
 
 test ('six divided by two is three', () => {
   expect(6 / 2).toBe(3);
 });
 
+
 test ('six multiplied by two is 12', () => {
   expect(6 * 2).toBe(12);
 });
+
 
 test ('six multiplied by two is 12', () => { // Duplicated the check to observe behavior of the doubled check
   expect(6 * 2).toBe(12);
 });
 
+
 test('two plus two is not four', () => { // fails by design (reason for failing: expected boolean when received number)
   expect(2 + 2).toBe(!4);
 });
 
+
 test('one plus one is not four', () => { // fails against design WIP (reason for failing: expected boolean when received number)
   expect(1 + 1).toBe(!4);
 });
+
 
 test('two plus two is not four', () => { // fails by design
   expect(2 + 2).not.toBe(4);
@@ -46,6 +53,7 @@ test('two plus two is not four', () => { // fails by design
 test('one plus two is not four', () => {
   expect(1 + 2).not.toBe(4);
 });
+
 
 test('one plus two is not four', () => {
   expect(1 + 2).not.toBe(4);
