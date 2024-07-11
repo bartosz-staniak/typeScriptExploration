50cb0e5c2459c4cc5c3685188bec9ca66ef218e4
bs <bartosz.staniak@gmail.com>
2024-07-11 13:34:20 +0200

    updated check's name and  received number

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -110,6 +110,6 @@ test('4 is close to 4.006', () => { // fails as expected difference is > 0.005
 });
 
 
-test('4 is close to 4.006', () => { // fails as expected difference is > 0.005
-  expect(4).toBeCloseTo(4.006);
+test('4 is close to 4.016', () => { // fails as expected difference is > 0.005
+  expect(4).toBeCloseTo(4.016);
 });
