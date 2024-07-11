a7fe9be86c8ecb8b65c1526fc051c6c002096d0d
bs <bartosz.staniak@gmail.com>
2024-07-11 12:59:22 +0200

    copied '5 is close to 6' check

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -98,3 +98,8 @@ test('5 is close to 6', () => { // fails as expected difference is < 0.005
 test('5 is not close to 6', () => {
   expect(5).not.toBeCloseTo(6);
 });
+
+
+test('5 is close to 6', () => { // fails as expected difference is < 0.005
+  expect(5).toBeCloseTo(6);
+});
