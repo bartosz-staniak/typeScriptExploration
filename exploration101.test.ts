cc5f41282ba75f82ec6a9925fcc7c23268cf8e25
bs <bartosz.staniak@gmail.com>
2024-07-12 01:24:37 +0200

    updated received difference comment

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -131,7 +131,7 @@ test('2 divided by 2 is close to 4.007', () => { // fails as expected difference
 
 
 test('0 is close to 0.005', () => { // fails as expected difference is > 0.005
-  expect(0).toBeCloseTo(0.005); // Received difference:   0.006
+  expect(0).toBeCloseTo(0.005); // Received difference:   0.005
 });
 
 
