f6f14366c5ee32f05db2683e740c504228c3b35a
bs <bartosz.staniak@gmail.com>
2024-07-13 01:42:40 +0200

    changed the name and expected value to 1.0051

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -241,8 +241,8 @@ test('1 is close to 1.005', () => { // passes unexpectedly as the difference is 
 });
 
 
-test('1 is close to 1.005', () => { // passes unexpectedly as the difference is not < 0.005
-  expect(1).toBeCloseTo(1.005);
+test('1 is close to 1.0051', () => { // passes unexpectedly as the difference is not < 0.005
+  expect(1).toBeCloseTo(1.0051);
 });
 
 
