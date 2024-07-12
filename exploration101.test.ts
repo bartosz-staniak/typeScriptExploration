073a9dfcfbfe78ea9c50dd0f808ff4241e73acd5
bs <bartosz.staniak@gmail.com>
2024-07-13 01:44:58 +0200

    changed the comment

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -242,7 +242,7 @@ test('1 is close to 1.005', () => { // passes unexpectedly as the difference is 
 
 
 test('1 is close to 1.0051', () => { // fails as expected difference is > 0.005
-  expect(1).toBeCloseTo(1.0051);
+  expect(1).toBeCloseTo(1.0051); // Received difference: 0.0051000000000001044
 });
 
 
