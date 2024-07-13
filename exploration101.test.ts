6144d37a53bc762b4fc0147ac65c33e48aa4f067
bs <bartosz.staniak@gmail.com>
2024-07-13 02:53:02 +0200

    copied a check

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -279,3 +279,8 @@ test('100 is close to 100.0051', () => { // fails as expected difference is > 0.
 test('1000 is close to 1000.005', () => { // passes unexpectedly as the difference is not < 0.005
   expect(1000).toBeCloseTo(1000.005);
 });
+
+
+test('1000 is close to 1000.005', () => { // passes unexpectedly as the difference is not < 0.005
+  expect(1000).toBeCloseTo(1000.005);
+});
