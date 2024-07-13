1488c1444c82d2512cfc908e827a2e6a71986d01
bs <bartosz.staniak@gmail.com>
2024-07-13 08:53:25 +0200

    changed check's name along with increase by 1 in received and expected values

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -256,8 +256,8 @@ test('1 is close to 1.007', () => { // fails as expected difference is > 0.005
 });
 
 
-test('1 is close to 1.005', () => { // passes unexpectedly as the difference is not < 0.005
-  expect(1).toBeCloseTo(1.005);
+test('2 is close to 2.005', () => { // passes unexpectedly as the difference is not < 0.005
+  expect(2).toBeCloseTo(2.005);
 });
 
 
