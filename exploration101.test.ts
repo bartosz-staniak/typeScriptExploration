c6fe38be8d608703a63121ba23a00773ced3787f
bs <bartosz.staniak@gmail.com>
2024-07-13 10:04:07 +0200

    changed check's name for better readibility

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -301,6 +301,6 @@ test('1,000,000 is close to 1,000,000.005', () => { // fails as expected but som
 });
 
 
-test('1000,000,000 is close to 1000,000,000.005', () => { // passes unexpectedly as the difference is not < 0.005
+test('1,000,000,000 is close to 1,000,000,000.005', () => { // passes unexpectedly as the difference is not < 0.005
   expect(1000000000).toBeCloseTo(1000000000.005);
 });
