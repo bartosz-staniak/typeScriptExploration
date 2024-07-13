26317090205434abd78de14c3b84b4b686fe7923
bs <bartosz.staniak@gmail.com>
2024-07-13 03:04:55 +0200

    changed the comment

--- exploration101.test.ts
+++ exploration101.test.ts
@@ -281,6 +281,6 @@ test('1000 is close to 1000.005', () => { // passes unexpectedly as the differen
 });
 
 
-test('1000,000 is close to 1000,000.005', () => { // passes unexpectedly as the difference is not < 0.005
+test('1000,000 is close to 1000,000.005', () => { // fails as expected but somehow the calculated difference is > 0.005
   expect(1000000).toBeCloseTo(1000000.005);
 });
