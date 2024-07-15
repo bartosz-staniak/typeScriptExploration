f3130c76f5c84799cb801d69c2d5151e8215b462
bs <bartosz.staniak@gmail.com>
2024-07-15 16:01:04 +0200

    commented out the variable

--- exploration101.ts
+++ exploration101.ts
@@ -22,7 +22,7 @@ function getTextDisplayDiv3() {
 
 function showTextInTextDisplay3() {
     // var returnedTextDisplayDiv3 = getTextDisplayDiv3();
-    let helloworld = "Hello world!";
+    // let helloworld = "Hello world!";
     var returnedTextDisplayDiv3 = document.getElementById("textDisplay3");
     if(returnedTextDisplayDiv3) returnedTextDisplayDiv3.innerHTML = helloworld; // prints 'undefined' on the page
 }
