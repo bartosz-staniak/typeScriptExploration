0f684f0246bc5ccd561a83682e51c838dfcd5e22
bs <bartosz.staniak@gmail.com>
2024-07-15 23:39:22 +0200

    removed a logging in console

--- exploration101.ts
+++ exploration101.ts
@@ -45,7 +45,6 @@ function showText() {
     oneMoreNumber += textToDisplay + someNumber; //results in NaN
 
     console.log(oneMoreNumber);
-    console.log(a);
 
     if (textDisplayDiv) textDisplayDiv.innerHTML = textToDisplay; // HTML element is not updated with global variable despite fixing not defined error for textDisplayDiv
 }
