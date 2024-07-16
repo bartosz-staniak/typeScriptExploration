7ec279d4437e996b4022b8941a6f612940ae03e4
bs <bartosz.staniak@gmail.com>
2024-07-16 16:08:13 +0200

    removed block contents

--- exploration101.ts
+++ exploration101.ts
@@ -5,9 +5,7 @@ export{} //solves duplicate function error yet breaks sum in tests and makes glo
 // var textDisplayDiv3 = document.getElementById("textDisplay3");
 var textDisplayDiv3;
 
-let object {
-    value: number // 'number' only refers to a type, but is being used as a value here.
-}
+let object {}
 function initialize() {
     object.value = 10;
 }
