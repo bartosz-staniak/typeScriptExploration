9a450640c8932a9c6603ab1e19cced8ab262685d
bs <bartosz.staniak@gmail.com>
2024-07-16 21:53:55 +0200

    failed attempt to log objectTs.value

--- exploration101.ts
+++ exploration101.ts
@@ -44,6 +44,7 @@ function showText() {
     oneMoreNumber += textToDisplay + someNumber; //results in NaN
 
     console.log(oneMoreNumber);
+    console.log(objectTs.value);
 
     if (textDisplayDiv) textDisplayDiv.innerHTML = textToDisplay; // HTML element is not updated with global variable despite fixing not defined error for textDisplayDiv
 }
