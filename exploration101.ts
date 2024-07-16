98d871fc5e6e0961cffb0d0857b021775a03bb2c
bs <bartosz.staniak@gmail.com>
2024-07-16 05:04:03 +0200

    commented out export {}

--- exploration101.ts
+++ exploration101.ts
@@ -1,4 +1,4 @@
-export{} //solves duplicate function error yet breaks sum in tests and makes global variables undefined
+// export{} //solves duplicate function error yet breaks sum in tests and makes global variables undefined
 // yet produces in the browser logs a "ReferenceError: exports is not defined"
 
 // var textDisplayDiv = document.getElementById("textDisplay"); // not defined error gone; perhaps it doesn't point to DOM somehow
